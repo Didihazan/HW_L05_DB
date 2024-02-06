bd669819ccf0316e321a89eb99e28c347517ef52
Yedidya Hazan <109093124+Didihazan@users.noreply.github.com>
2024-02-06 13:02:40 +0200

    Update shows.js

--- hw_questions/routs/shows.js
+++ hw_questions/routs/shows.js
@@ -14,8 +14,65 @@ router.get("/",async(req,res) => {
     // 502 - שגיאה בשרת
     res.status(502).json({err:"there problem , try again later"})
   }
-  // res.json({msg:"players work 2222"})
 })
 
-// export default
+// בקשת פוסט להוספת רשומה חדשה
+router.post("/",async(req,res) => {
+  // בדיקה תקינות לבאדי
+  const validBody = validateShows(req.body);
+  // בודק אם יש שגיאה
+  if(validBody.error){
+    return res.status(400).json(validBody.error.details);
+  }
+
+  try{
+    const shows = new ShowsModel(req.body);
+    // שומר את הרשומה
+    await shows.save();
+    res.json(shows);
+ 
+  }
+  catch(err){
+    console.log(err);
+    res.status(502).json({err})
+  }
+})
+
+// עריכת רשומה
+router.put("/:id",async(req,res) => {
+  // בדיקה תקינות לבאדי
+  const validBody = validateShows(req.body);
+  // בודק אם יש שגיאה
+  if(validBody.error){
+    // מחזיר את השגיאה המודייקת שיש בבאדי לפי הסכמה
+    return res.status(400).json(validBody.error.details);
+  }
+  try{
+    const id = req.params.id;
+    // בקשת עדכון לפי מאפיין האיי די
+    const data = await ShowsModel.updateOne({_id:id},req.body)
+    // modfiedCount:1 אם הצליח למחוק
+    res.json(data)
+  }
+  catch(err){
+    console.log(err);
+    res.status(502).json({err})
+  }
+})
+
+// מחיקת רשומה
+router.delete("/:id",async(req,res) => {
+  try{
+    const id = req.params.id;
+    // בקשת מחיקה לפי מאפיין האיי די
+    const data = await ShowsModel.deleteOne({_id:id})
+    // deletedCount:1 אם הצליח למחוק
+    res.json(data)
+  }
+  catch(err){
+    console.log(err);
+    res.status(502).json({err})
+  }
+})
+
 module.exports = router;
